0e444712abe02d484c6583d10bb3340a28976c64
Pritam Sangani <pritam.sangani@thoughtworks.com>
2021-09-24 13:14:45 +0100

    #7 - fixes test by wrapping app in FirebaseAppProvider

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { FirebaseAppProvider } from 'reactfire';
+import { firebaseConfig } from '../config';
 
 describe('App', () => {
   function renderApp(route: string) {
     window.history.replaceState({}, 'Test page', route);
-    return render(<App />);
+    return render(
+      <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+        <App />
+      </FirebaseAppProvider>,
+    );
   }
 
   test('should display progress trackers page', async () => {
