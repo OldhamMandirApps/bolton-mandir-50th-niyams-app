c5d79b54c3798997b4b1f3db4bef909c65a3cf41
Pritam Sangani <pritam.sangani@thoughtworks.com>
2021-09-26 15:51:11 +0100

    #15 - adds page heading to progress trackers page

--- src/pages/ProgressTrackersPage/ProgressTrackersPage.tsx
+++ src/pages/ProgressTrackersPage/ProgressTrackersPage.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Niyam } from '../../config/niyams';
 import ProgressTracker from '../../components/ProgressTracker';
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 
 function ProgressTrackersPage(): JSX.Element {
   return (
     <Container data-testid='progress-trackers-page' maxWidth={false} sx={{ mt: 2, p: 2, mx: 0 }}>
+      <Typography variant='h4' component='h1' sx={{ fontWeight: 700, mb: 4 }}>
+        Niyam Progress
+      </Typography>
       <Grid data-testid='trackers' container direction='column' spacing={2}>
         <ProgressTracker niyam={Niyam.ShantiPaath} />
         <ProgressTracker niyam={Niyam.JanmangalNamavali} />
