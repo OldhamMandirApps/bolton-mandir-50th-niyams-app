55a7b04f21878caf420c5053a325d86dc569284d
Pritam Sangani <pritam.sangani@thoughtworks.com>
2021-10-09 14:24:32 +0100

    #23 - adds all niyams to menu list for selecting niyam

--- src/pages/AddNiyamsPage/AddNiyamProgressForm/fields/NiyamSelect/NiyamSelect.test.tsx
+++ src/pages/AddNiyamsPage/AddNiyamProgressForm/fields/NiyamSelect/NiyamSelect.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import NiyamSelect from './NiyamSelect';
+import userEvent from '@testing-library/user-event';
 
 describe('NiyamSelect', () => {
   function renderSelectField() {
@@ -10,5 +11,17 @@ describe('NiyamSelect', () => {
     renderSelectField();
 
     screen.getByRole('button', { name: /select niyam/i });
+  });
+
+  test('should display all niyams in drop-down list when clicking on select field', () => {
+    renderSelectField();
+
+    userEvent.click(screen.getByRole('button', { name: /select niyam/i }));
+
+    screen.getByRole('option', { name: /shanti paath/i });
+    screen.getByRole('option', { name: /janmangal namavali/i });
+    screen.getByRole('option', { name: /janmangal stotram/i });
+    screen.getByRole('option', { name: /orada na pads/i });
+    screen.getByRole('option', { name: /utsav kirtan/i });
   });
 });
--- src/pages/AddNiyamsPage/AddNiyamProgressForm/fields/NiyamSelect/NiyamSelect.tsx
+++ src/pages/AddNiyamsPage/AddNiyamProgressForm/fields/NiyamSelect/NiyamSelect.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { FormControl, Grid, InputLabel, MenuItem, Select } from '@mui/material';
+import { Niyam } from '../../../../../config/niyams';
 
 function NiyamSelect(): JSX.Element {
+  const niyams = useMemo(() => {
+    return Object.values(Niyam);
+  }, []);
+
   return (
     <Grid item>
       <FormControl fullWidth data-testid='niyam-select-field'>
         <InputLabel id='select-niyam-label'>Select Niyam</InputLabel>
         <Select labelId='select-niyam-label' id='select-niyam' label='Select Niyam'>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {niyams.map((niyam) => (
+            <MenuItem key={niyam} value={niyam}>
+              {niyam}
+            </MenuItem>
+          ))}
         </Select>
       </FormControl>
     </Grid>
