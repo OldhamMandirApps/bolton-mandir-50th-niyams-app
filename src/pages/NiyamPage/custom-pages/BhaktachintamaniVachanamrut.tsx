41dbd16e97ef7d4b6db12dd2867aad5eaa036356
Pritam Sangani <pritam.sangani@thoughtworks.com>
2021-11-02 13:26:47 +0000

    fixes formatting issues for bhaktachintamani / vachanamrut page

--- src/pages/NiyamPage/custom-pages/BhaktachintamaniVachanamrut.tsx
+++ src/pages/NiyamPage/custom-pages/BhaktachintamaniVachanamrut.tsx
@@ -9,30 +9,29 @@ function BhaktachintamaniVachanamrut(): JSX.Element {
       <Navbar showLanguageToggle={false} />
       <PageContainer data-testid='bhaktachintamani-vachanamrut'>
         <H1>Bhaktachintamani/Vachanamrut</H1>
-        <Box pt='16px'>
-          <Typography fontWeight='medium' pt='8px' fontSize='18px'>
+        <Box pt='16px' width='100%' sx={{ overflowWrap: 'break-word', wordWrap: 'break-word' }}>
+          <Typography fontWeight='medium' fontSize='18px' pt='8px'>
             As part of our niyams for our Nutan Mandir Mahotsav, we have set a target to read 450 paaths of the
             Bhaktachintamani and Vachanamrut.
           </Typography>
-          <Typography fontWeight='medium' pb='8px' fontSize='18px'>
+          <Typography pb='8px' mt='4px' sx={{ color: 'gray', fontWeight: '600', fontSize: '18px' }}>
             Click on the links below to read the granth of your choice:
           </Typography>
-          <Typography fontWeight='medium' pt='16px' pb='8px' fontSize='18px'>
-            Vachanamrut:
+          <Typography pt='16px' pb='8px' sx={{ textDecoration: 'underline', fontWeight: 'medium', fontSize: '18px' }}>
+            Vachanamrut
           </Typography>
-          <Typography fontWeight='medium'>
-            Play store:
-            <Link href='https://play.google.com/store/apps/details?id=com.bhujmandir.vachanamrut' ml='8px'>
-              https://play.google.com/store/apps/details?id=com.bhujmandir.vachanamrut
-            </Link>
-            <br />
+          <Typography fontWeight='medium'>Play store:</Typography>
+          <Link href='https://play.google.com/store/apps/details?id=com.bhujmandir.vachanamrut' fontWeight='medium'>
+            https://play.google.com/store/apps/details?id=com.bhujmandir.vachanamrut
+          </Link>
+          <Typography fontWeight='medium' mt='8px'>
             App Store:
-            <Link href='https://apps.apple.com/us/app/vachanamrut-learning-app/id1476077803?ls=1' ml='8px'>
-              https://apps.apple.com/us/app/vachanamrut-learning-app/id1476077803?ls=1
-            </Link>
           </Typography>
-          <Typography fontWeight='medium' pt='18px' pb='8px' fontSize='18px'>
-            Bhaktachintamani:
+          <Link href='https://apps.apple.com/us/app/vachanamrut-learning-app/id1476077803?ls=1' fontWeight='medium'>
+            https://apps.apple.com/us/app/vachanamrut-learning-app/id1476077803?ls=1
+          </Link>
+          <Typography pt='18px' pb='8px' sx={{ textDecoration: 'underline', fontWeight: 'medium', fontSize: '18px' }}>
+            Bhaktachintamani
           </Typography>
           <Link href='https://www.swaminarayan.faith/media/3902/bhakta-chintamni.pdf' fontWeight='medium'>
             https://www.swaminarayan.faith/media/3902/bhakta-chintamni.pdf
