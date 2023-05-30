a94eb0e3492e7bac6c127e0449d2e98abe267d4b
Pritam Sangani <pritam.sangani@gmail.com>
2023-05-30 14:01:15 +0100

    use short form for mandir name

--- src/pages/common/Navbar.tsx
+++ src/pages/common/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar(): JSX.Element {
       <Toolbar sx={{ backgroundColor: '#BC3606', py: '8px' }}>
         <Grid container alignItems='center' flexDirection='column'>
           <Typography sx={{ color: '#EDBC63', fontSize: '14px', fontWeight: 600, textAlign: 'center' }}>
-            Shree Kutch Satsang Swaminarayan Temple Bolton
+            S.K.S Swaminarayan Temple Bolton
           </Typography>
           <Typography sx={{ color: '#EDBC63', fontSize: '14px', fontWeight: 400 }}>
             Under Shree Nar Narayan Dev Bhuj
