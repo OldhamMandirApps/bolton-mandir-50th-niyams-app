ee94faac1874d83a847507b1469663eb93927974
Pritam Sangani <pritam.sangani@gmail.com>
2023-05-30 13:51:52 +0100

    test out font size to make mandir name fit on one line

--- src/pages/common/Navbar.tsx
+++ src/pages/common/Navbar.tsx
@@ -12,10 +12,10 @@ function Navbar(): JSX.Element {
     <>
       <Toolbar sx={{ backgroundColor: '#BC3606', py: '8px' }}>
         <Grid container alignItems='center' flexDirection='column'>
-          <Typography sx={{ color: '#EDBC63', fontSize: '24px', fontWeight: 600, textAlign: 'center' }}>
+          <Typography sx={{ color: '#EDBC63', fontSize: '14px', fontWeight: 600, textAlign: 'center' }}>
             Shree Kutch Satsang Swaminarayan Temple Bolton
           </Typography>
-          <Typography sx={{ color: '#EDBC63', fontSize: '18px', fontWeight: 400 }}>
+          <Typography sx={{ color: '#EDBC63', fontSize: '14px', fontWeight: 400 }}>
             Under Shree Nar Narayan Dev Bhuj
           </Typography>
         </Grid>
